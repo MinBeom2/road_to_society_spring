28582315421745d8719b9ea0260d23c34351fa14
MinBeom2 <alsqja104@gmail.com>
2024-08-01 03:57:38 +0900

    0731

---
+++ ch13/sq-ch13-ex1/src/main/java/example/main/repositories/AccountRepository.java
@@ -0,0 +1,35 @@
+package example.main.repositories;
+
+
+import example.main.model.Account;
+import example.main.repositories.mappers.AccountRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+@Repository
+public class AccountRepository {
+
+    private final JdbcTemplate jdbc;
+
+    public AccountRepository(JdbcTemplate jdbc) {
+        this.jdbc = jdbc;
+    }
+
+    public Account findAccountById(long id) {
+        String sql = "SELECT * FROM account WHERE id = ?";
+        return jdbc.queryForObject(sql, new AccountRowMapper(), id);
+    }
+
+    public List<Account> findAllAccounts() {
+        String sql = "SELECT * FROM account";
+        return jdbc.query(sql, new AccountRowMapper());
+    }
+
+    public void changeAmount(long id, BigDecimal amount){
+        String sql = "UPDATE account SET amount = ? WHERE id = ?";
+        jdbc.update(sql,amount,id);
+    }
+}
